d5f967616c17776783bae862f1f616a84b42cac8
DenielMay <56801154+DenielMay@users.noreply.github.com>
2022-05-14 11:18:45 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'django_filters',
     'reviews',
     'api',
