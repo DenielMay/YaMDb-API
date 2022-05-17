2ef778333bafdc4a3398e8d3d3f9348d2bf4dac2
Игорь Иштубаев <ig.isht@gmail.com>
2022-05-17 12:39:28 +0300

    added docstring

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,6 +3,7 @@ from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
+    """Фильтрация по слагам категориии/жанра, названию произведения и году"""
     category = django_filters.CharFilter(field_name="category__slug")
     genre = django_filters.CharFilter(field_name="genre__slug")
     name = django_filters.CharFilter(field_name="name", lookup_expr='contains')
